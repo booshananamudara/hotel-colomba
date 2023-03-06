e4f34da7a634c77b169ed077f6f865ab9ce44d51
Booshana Namudara <booshiwork@gmail.com>
2023-03-06 15:34:36 +0530

    create full home page

---
+++ resources/js/script.js
@@ -0,0 +1,7 @@
+// ----------------------- typing animation -----------------------------------------
+var typed = new Typed(".typing",{
+    strings:["","COLOMBA"],
+    typeSpeed:130,
+    BackSpeed:70,
+    loop:true
+})
